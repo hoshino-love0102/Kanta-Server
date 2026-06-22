c5ed85ec1b03eeb2a6f4b6b74c7ad7adc3839dd5
hoshino-love0102 <hoshino.whale0102@gmail.com>
2026-06-22 20:23:53 +0900

    feat(github): 인메모리 rate limiter 코어 클래스 추가

---
+++ service-github/src/main/java/com/kanta/github/infrastructure/ratelimit/RateLimitResult.java
@@ -0,0 +1,11 @@
+package com.kanta.github.infrastructure.ratelimit;
+
+public record RateLimitResult(boolean allowed, long retryAfterSeconds) {
+    public static RateLimitResult permit() {
+        return new RateLimitResult(true, 0);
+    }
+
+    public static RateLimitResult exceeded(long retryAfterSeconds) {
+        return new RateLimitResult(false, retryAfterSeconds);
+    }
+}
