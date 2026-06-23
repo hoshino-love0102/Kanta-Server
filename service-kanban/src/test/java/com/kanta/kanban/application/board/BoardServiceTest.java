b63f856e9c53d83960c4e91498c702b05adf17be
hoshino-love0102 <hoshino.whale0102@gmail.com>
2026-06-23 14:02:45 +0900

    test(kanban): listByWorkspace 테스트 추가

--- service-kanban/src/test/java/com/kanta/kanban/application/board/BoardServiceTest.java
+++ service-kanban/src/test/java/com/kanta/kanban/application/board/BoardServiceTest.java
@@ -11,6 +11,7 @@ import com.kanta.kanban.common.NotFoundException;
 import com.kanta.kanban.domain.board.entity.Board;
 import com.kanta.kanban.domain.board.repository.BoardRepository;
 import com.kanta.kanban.presentation.board.CreateBoardRequest;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
@@ -72,5 +73,27 @@ class BoardServiceTest {
 
         assertThatThrownBy(() -> boardService.findBoard(boardId))
             .isInstanceOf(NotFoundException.class);
+    }
+
+    @Test
+    void 워크스페이스의_보드_목록을_조회한다() {
+        var workspaceId = UUID.randomUUID();
+        var board = new Board(workspaceId, "board");
+        when(boardRepository.findByWorkspaceId(workspaceId)).thenReturn(List.of(board));
+
+        var responses = boardService.listByWorkspace(workspaceId);
+
+        assertThat(responses).hasSize(1);
+        assertThat(responses.get(0).name()).isEqualTo("board");
+    }
+
+    @Test
+    void 보드가_없는_워크스페이스는_빈_목록을_반환한다() {
+        var workspaceId = UUID.randomUUID();
+        when(boardRepository.findByWorkspaceId(workspaceId)).thenReturn(List.of());
+
+        var responses = boardService.listByWorkspace(workspaceId);
+
+        assertThat(responses).isEmpty();
     }
 }
