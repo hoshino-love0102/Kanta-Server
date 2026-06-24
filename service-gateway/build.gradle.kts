5ee247e0ba536ab5ef1c080f4354572df03d7b7d
hoshino-love0102 <hoshino.whale0102@gmail.com>
2026-06-24 22:59:07 +0900

    feat(gateway): actuator health/env 엔드포인트 노출

--- service-gateway/build.gradle.kts
+++ service-gateway/build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
